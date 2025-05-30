3739d6675ee43c6227b45c25b06a6ac42c7fc913
juan <juan@gmail.com>
2025-05-29 21:58:02 -0500

    Primer commit

---
+++ src/backend/signup.php
@@ -0,0 +1,45 @@
+<?php
+
+include('../config/database.php');
+
+$fname=$_POST['f_name'];
+$lname=$_POST['l_name'];
+$email=$_POST['e_mail'];
+$passw=$_POST['p_assw'];
+
+//$hashed_password=password_hash($passw,PASSWORD_DEFAULT);
+$hashed_password=$passw;
+
+$sql_validate_email = "
+ select 
+  count (id) as total
+ from
+   users
+ where 
+   email='$email' and
+   status=true
+ ";
+ $ans = pg_query($conn, $sql_validate_email);
+
+ if ($ans) {
+    $row = pg_fetch_assoc($ans);
+    if ($row['total'] > 0) {
+        echo "user already exists"; 
+    } else {
+    $sql = "INSERT INTO users
+     (firstname, lastname, email, password) 
+     VALUES ('$fname', '$lname', '$email', '$passw')";
+
+    $ans = pg_query($conn, $sql);
+    if  ($ans) {    
+        echo "<script>alert ('user has been created . Go to login! ')</script>";
+        header('refresh:0;url=http://localhost/pet-store2/src/singin.html');
+    } else {
+        echo " error ";
+    }}
+}else {
+        echo "query Error ";
+    }
+
+
+?>
---
+++ src/config/database.php
@@ -0,0 +1,35 @@
+<?php
+
+    $host        ="aws-0-us-east-1.pooler.supabase.com";
+    $port        ="6543";
+    $dbname      ="postgres";
+    $user        ="postgres.aurfizzllfzhjanppijz";
+    $password    ="unicesmag@@";
+    
+    /*
+    $host        ="localhost";
+    $port        ="5432";
+    $dbname      ="petstore";
+    $user        ="postgres";
+    $password    ="unicesmag";
+    */
+    $data_connection="
+        host=$host
+        port=$port
+        dbname=$dbname
+        user=$user
+        password=$password
+    ";
+
+    // Crear la conexión
+    $conn = pg_connect($data_connection);
+
+    if(!$conn){
+        echo "Connection error";
+    }else{
+        echo "Success !!!";
+    }
+
+    //pg_close($conn);
+
+?>
